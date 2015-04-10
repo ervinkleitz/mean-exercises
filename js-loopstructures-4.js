b895e61fd6cc34141ec8fb4e067828012f1057b3
Ervin Gonzales <ervinkleitz@gmail.com>
2015-04-09 20:42:07 -0700

    Shortened loop

--- js-loopstructures-4.js
+++ js-loopstructures-4.js
@@ -11,15 +11,12 @@ for ( var i = 1; i <= 10; i++ ) {
 }
 
 // Checks each number in 'array' against the others
-for ( var j = 0; j < array.length; j++ ) {
-
 	for (var k = 0; k < array.length; k++ ) {
 
-		if ( array[j] < array[k] && array[j] < array[lowest]) {
-			lowest = j;
+		if ( array[k] < array[lowest]) {
+			lowest = k;
 		}
 	}
 
-}
 // Prints out the lowest number
 console.log( 'The lowest number in the list is: ' + array[lowest]);
