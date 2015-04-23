994d0510cedab18160dd212dd025eae15226e566
Ervin Gonzales <ervinkleitz@gmail.com>
2015-04-22 19:58:24 -0700

    Added another button for next page

--- week3/tamagotchi_exercise/script.js
+++ week3/tamagotchi_exercise/script.js
@@ -1,28 +1,31 @@
-	var print = '';
+$('#namepage').hide();
 
-	print += 'Egg hunter! You found nest of 5 eggs! <br><br>';
+var print = '';
 
-	print += '(Hairy Egg, Moldy Egg, Egg with eyeballs, Snoring Egg, Egg with Teeth) <br><br>';
-	
-	var eggType = Math.ceil( Math.random() * 5 );
+print += 'Egg hunter! You found nest of 5 eggs! <br><br>';
 
-		switch ( eggType ) {
+print += '(Hairy Egg, Moldy Egg, Egg with eyeballs, Snoring Egg, Egg with Teeth) <br><br>';
 
-			case 1: eggType = 'Hairy Egg'; break;
-			case 2: eggType = 'Moldy Egg'; break;
-			case 3: eggType = 'Egg with eyeballs'; break;
-			case 4: eggType = 'Snoring Egg'; break;
-			case 5: eggType = 'Egg with Teeth'; break;
+var eggType = Math.ceil( Math.random() * 5 );
 
-		}
+	switch ( eggType ) {
 
-	print += "For some reason, the " + eggType + " calls out to you and you put it in your bag.";
-	print += "<br><br>A few days later, your egg hatches!";
+		case 1: eggType = 'Hairy Egg'; break;
+		case 2: eggType = 'Moldy Egg'; break;
+		case 3: eggType = 'Egg with eyeballs'; break;
+		case 4: eggType = 'Snoring Egg'; break;
+		case 5: eggType = 'Egg with Teeth'; break;
+
+	}
+
+print += "For some reason, the " + eggType + " calls out to you and you put it in your bag.";
+print += "<br><br>A few days later, your egg hatches!";
 
 var start = function() {
 
 		document.getElementById("output").innerHTML = print;
 		$('#start').hide();
+		$('#namepage').show();
 
 };
 
