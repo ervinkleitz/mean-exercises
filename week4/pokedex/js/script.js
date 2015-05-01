3ab510fc09f4283a456ae01c9b121b6fad2bf5f0
Ervin Gonzales <ervinkleitz@gmail.com>
2015-05-01 02:10:34 -0700

    Default dropdown and some pokemon types done

--- week4/pokedex/js/script.js
+++ week4/pokedex/js/script.js
@@ -3,22 +3,43 @@ $(document).ready(function(){
     $.ajax('http://pokeapi.co/api/v1/pokedex/1/', {
       method: 'GET'
     })
-    .done(handle_request)
+    .done( populateDropdown )
     .fail( function () {
       console.log('fail');
     });
-
-    function handle_request( data ){
+    
+    $.ajax('http://pokeapi.co/api/v1/pokemon/1/', {
+      method: 'GET'
+    })
+    .done(populateFirstPokemon)
+    .fail( function () {
+      console.log('fail');
+    });
+    
+    var pokemonName, firstPokemon, pokemonFirstLetter;
+    
+    //Function to populate dropdown list
+    function populateDropdown ( data ){
+        console.log(data);
         var pokemonList = '';
-        var firstPokemon = data[ 'pokemon' ][0][ 'name' ]
+        
         for ( var index = 0; index < data[ 'pokemon' ].length; index++ ){
             pokemonName = data[ 'pokemon' ][index][ 'name' ];
+            pokemonNameFirstLetter = pokemonName[0].toUpperCase();
+            pokemonName = pokemonNameFirstLetter + pokemonName.substr(1);
             pokemonList += '<li><a href="#">' + pokemonName + '</a></li>';
         }
-        console.log( data );
+        
+        //Populates dropdown list and first pokemon
         document.getElementById( 'pokemon-dropdown' ).innerHTML = firstPokemon + '  <span class="caret"></span>';
         document.getElementById( 'pokemon-list' ).innerHTML = pokemonList;
-        
     }
+    
+    function populateFirstPokemon ( data ) {
+        pokemonName = data['name'];
+        document.getElementById( 'name' ).innerHTML = 'Name: ' + pokemonName + '<br>';
+        document.getElementById( 'pokemon-dropdown' ).innerHTML = pokemonName + ' <span class="caret"></span>';
+    }
+
 
 });
