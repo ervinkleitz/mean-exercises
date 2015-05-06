3cb997cdb60f17964d2c56893ac5c3bce3f51a8f
Ervin Gonzales <ervinkleitz@gmail.com>
2015-05-05 17:05:19 -0700

    Added button for post

--- week5/bounty.js
+++ week5/bounty.js
@@ -23,29 +23,39 @@ var bounty = [{
 	];
 
 app.post('/bounty', function(request, response){
+	request.body.id = bounty.length + 1;
 	bounty.push(request.body);
 	response.send('Data Saved');
 });
 
 app.get('/bounty', function(request, response){
 	var sendToHtml = '';
+	
+	
+	//Wanted img
 	sendToHtml += '<img src="http://web.redding.com/images/misc/wanted-responsive.png" style="display: block; margin: 0 auto;';
-	sendToHtml += 'height: 5em;">';
+	sendToHtml += 'height: 5em;"><button style="background-color: black; color: white; display: block; margin: 0 auto;"';
+	sendToHtml += 'id="add">Add Bounty</button>';
 	//Loops through all the Bounties
 	for ( var numberOfBounties = 0; numberOfBounties < bounty.length ; numberOfBounties++ ){
-		sendToHtml += '<hr style="width: 10em;"><p style="text-align: center; color: lightblue; font-family: sans-serif;">Bounty number: ';
+		sendToHtml += '<body style="font-family: sans-serif; background-color: lightred;"><div id="post-form">';
+		sendToHtml += '<hr style="width: 10em;"><p style="text-align: center; color: lightblue;">Bounty number: ';
 		sendToHtml += ( numberOfBounties + 1 ) + '</p><hr style="width: 10em;">';
 		//Bounty (listed)
-		sendToHtml += '<ul style="list-style-position:inside;width: 13em; display:block;margin: 0 auto;">';
-		sendToHtml += '<li style="font-family:sans-serif;">First Name: <b>' + bounty[numberOfBounties]['firstname'] + '</b></li>';
-		sendToHtml += '<li style="font-family:sans-serif;">Last Name: <b>' + bounty[numberOfBounties]['lastname'] + '</b></li>';
-		sendToHtml += '<li style="font-family:sans-serif;">Living: <b>' + bounty[numberOfBounties]['living'] + '</b></li>';
-		sendToHtml += '<li style="font-family:sans-serif;">Bounty: <b>' + bounty[numberOfBounties]['bounty'] + '</b></li>';
-		sendToHtml += '<li style="font-family:sans-serif;">Type: <b>' + bounty[numberOfBounties]['type'] + '</b></li>';
-		sendToHtml += '</ul>';
-	}
+		sendToHtml += '<ul style="list-style-position: inside; width: 13em; display: block; margin: 0 auto;">';
+		sendToHtml += '<li>First Name: <b>' + bounty[numberOfBounties]['firstname'] + '</b></li>';
+		sendToHtml += '<li>Last Name: <b>' + bounty[numberOfBounties]['lastname'] + '</b></li>';
+		sendToHtml += '<li>Living: <b>' + bounty[numberOfBounties]['living'] + '</b></li>';
+		sendToHtml += '<li>Bounty: <b>' + bounty[numberOfBounties]['bounty'] + '</b></li>';
+		sendToHtml += '<li>Type: <b>' + bounty[numberOfBounties]['type'] + '</b></li>';
+		sendToHtml += '</ul></div></body>';
+	}//End of loop
 	
 	response.send(sendToHtml);
+	document.getElementById( 'add' ).addEventListener( 'click', postToServer );
+	
+	function postToServer(){
+	}
 });
 
 app.listen(5000);
