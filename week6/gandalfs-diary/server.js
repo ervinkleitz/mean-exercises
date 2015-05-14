7d967a157b9c1485522e34ee5f7ba5450728c0ed
Ervin Gonzales <ervinkleitz@gmail.com>
2015-05-13 23:46:20 -0700

    Updated server

--- week6/gandalfs-diary/server.js
+++ week6/gandalfs-diary/server.js
@@ -2,12 +2,25 @@ var express = require('express');
 var app = express();
 var bodyParser = require('body-parser');
 var PORT = process.env.PORT || 8080;
+var model = require('.model.js');
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static(__dirname));
 
+app.get('/save', function(req,res){
+	journalEntry.findOne({}, function(err,data){
+		response.send(data);
+	});
+});
 
+app.post('/save', function(req,res){
+	journalEntry.findOne({}, function(err,data){
+		data.entry = request.body.clientEntry;
+		data.save();
+		response.send();
+	});
+});
 
 app.listen(PORT);
 console.log('Listening on port ' + PORT);
